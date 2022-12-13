12140243ccfe9646de903ee5736913aceeaa276b
Bilal Ashraf <bilalashraf6233@gmail.com>
2022-12-14 01:04:04 +0500

    first commit

---
+++ build/precache-manifest.974f18e0ac7a333748065735d5062cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c84d7e37c40ff4b1f56",
+    "url": "/static/js/main.3c84d7e3.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cf6aab8c49e0b1091ca4",
+    "url": "/static/js/2.cf6aab8c.chunk.js"
+  },
+  {
+    "revision": "b23d5960ea1aa29a34d567b62f9d1617",
+    "url": "/static/media/avatar.b23d5960.jpg"
+  },
+  {
+    "revision": "586806cd047e33b5ee7a43fc4252208d",
+    "url": "/static/media/mountains.586806cd.jpg"
+  },
+  {
+    "revision": "44c4085faca9fc29c21044b82f708bee",
+    "url": "/index.html"
+  }
+];
